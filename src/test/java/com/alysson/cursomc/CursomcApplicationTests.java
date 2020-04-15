d7972b61eb93a6eb97ecb6eb79f85e577358aceb
Alysson Kelvim <akcsalysson@gmail.com>
2020-04-15 01:28:42 -0300

    Projeto criado

---
+++ src/test/java/com/alysson/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alysson.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
