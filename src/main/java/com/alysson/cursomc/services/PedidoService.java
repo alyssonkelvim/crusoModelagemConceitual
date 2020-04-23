1d75a5134ae0f3c76db6ddc37234c36bae5c05f9
Alysson Kelvim <akcsalysson@gmail.com>
2020-04-23 16:05:18 -0300

    Endpoint Pedidos

---
+++ src/main/java/com/alysson/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.alysson.cursomc.services;
+
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alysson.cursomc.domain.Pedido;
+import com.alysson.cursomc.repositories.PedidoRepository;
+import com.alysson.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
