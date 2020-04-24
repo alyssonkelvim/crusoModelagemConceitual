6a937aa552a0f66faac7f475c6f6f5efdd5502eb
Alysson Kelvim <akcsalysson@gmail.com>
2020-04-24 19:00:16 -0300

    CRUD: PUT, GET e DELETE para Cliente

---
+++ src/main/java/com/alysson/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,68 @@
+package com.alysson.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.alysson.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=180, message="O tamanho deve ser entre 5 e 80 caracteres")
+	private String nome;
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Email(message="E-mail Inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		this.id = obj.getId();
+		this.email = obj.getEmail();
+		this.nome = obj.getNome();
+	}
+
+	public ClienteDTO(Integer id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
--- src/main/java/com/alysson/cursomc/services/ClienteService.java
+++ src/main/java/com/alysson/cursomc/services/ClienteService.java
@@ -1,13 +1,20 @@
 package com.alysson.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alysson.cursomc.domain.Cliente;
+import com.alysson.cursomc.dto.ClienteDTO;
 import com.alysson.cursomc.repositories.ClienteRepository;
+import com.alysson.cursomc.services.exceptions.DataIntegrityException;
 import com.alysson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,5 +27,40 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente oldObj = find(obj.getId());
+		obj = updateData(obj, oldObj);
+		return repo.save(obj);
+	}
+
+
+
+	public void delete(Integer id) {
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(null, objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	private Cliente updateData(Cliente obj, Cliente oldObj) {
+		oldObj.setEmail(obj.getEmail());
+		oldObj.setNome(obj.getNome());
+		return oldObj;
 	}
 }
