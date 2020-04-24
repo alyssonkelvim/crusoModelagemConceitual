9a2377cfbd307fdec76861d7f8776c226a47d721
Alysson Kelvim <akcsalysson@gmail.com>
2020-04-24 15:36:22 -0300

    CRUD: Validação Sintática com Bean Validation

--- src/main/java/com/alysson/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/alysson/cursomc/dto/CategoriaDTO.java
@@ -2,13 +2,25 @@ package com.alysson.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.alysson.cursomc.domain.Categoria;
+
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=8, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
+	
+	public CategoriaDTO() {
+		
+	}
 	
 	public CategoriaDTO(Categoria obj) {
 		this.id = obj.getId();
@@ -26,6 +38,31 @@ public class CategoriaDTO implements Serializable {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoriaDTO other = (CategoriaDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	
