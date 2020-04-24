554b5a452abd34c5892beb66fb41eeba45b809e8
Alysson Kelvim <akcsalysson@gmail.com>
2020-04-24 13:14:13 -0300

    CRUD: Excluindo Categoria

--- src/main/java/com/alysson/cursomc/services/CategoriaService.java
+++ src/main/java/com/alysson/cursomc/services/CategoriaService.java
@@ -2,12 +2,13 @@ package com.alysson.cursomc.services;
 
 import java.util.Optional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.alysson.cursomc.domain.Categoria;
 import com.alysson.cursomc.repositories.CategoriaRepository;
+import com.alysson.cursomc.services.exceptions.DataIntegrityException;
 import com.alysson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,14 +22,22 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
