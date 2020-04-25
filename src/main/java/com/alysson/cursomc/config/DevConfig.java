c4490994ffc265d69600f88a71f150cc90263c18
Alysson Kelvim <akcsalysson@gmail.com>
2020-04-25 19:52:56 -0300

    BD: Criando profile de Desenvolvimento

---
+++ src/main/java/com/alysson/cursomc/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.alysson.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.alysson.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instatiateDatabase() throws ParseException {
+		if(strategy.equals("create")) {
+			dbService.instatiateTestDatabase();
+			return true;
+		}
+		return false;
+		
+	}
+}
