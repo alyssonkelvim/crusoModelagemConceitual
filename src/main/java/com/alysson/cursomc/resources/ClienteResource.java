0e7c7a95c6de5bef71a7a285831a23e66bbf2abd
Alysson Kelvim <akcsalysson@gmail.com>
2020-04-25 00:00:06 -0300

    CRUD: POST - Inserindo novo cliente

--- src/main/java/com/alysson/cursomc/resources/ClienteResource.java
+++ src/main/java/com/alysson/cursomc/resources/ClienteResource.java
@@ -1,7 +1,6 @@
 package com.alysson.cursomc.resources;
 
-
-
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,25 +15,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alysson.cursomc.domain.Cliente;
-import com.alysson.cursomc.domain.Cliente;
 import com.alysson.cursomc.dto.ClienteDTO;
+import com.alysson.cursomc.dto.ClienteNewDTO;
 import com.alysson.cursomc.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/clientes")
+@RequestMapping(value = "/clientes")
 public class ClienteResource {
-	
+
 	@Autowired
 	private ClienteService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok(obj);
 	}
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id) {
 		Cliente obj = service.fromDTO(objDTO);
@@ -57,17 +65,13 @@ public class ClienteResource {
 	}
 
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
-	public ResponseEntity<Page<ClienteDTO>> findPage(
-			@RequestParam(value="page", defaultValue = "0")Integer page, 
-			@RequestParam(value="linesPerPage", defaultValue = "24")Integer linesPerPage, 
-			@RequestParam(value="orderBy", defaultValue = "nome")String orderBy,
-			@RequestParam(value="direction", defaultValue = "ASC" )String direction
-			) {
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 		Page<Cliente> pages = service.findPage(page, linesPerPage, orderBy, direction);
-		Page<ClienteDTO> pagesDto = pages.map(obj -> new ClienteDTO(obj)); 
+		Page<ClienteDTO> pagesDto = pages.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(pagesDto);
 	}
 
 }
-
-
--- src/main/java/com/alysson/cursomc/services/ClienteService.java
+++ src/main/java/com/alysson/cursomc/services/ClienteService.java
@@ -3,25 +3,35 @@ package com.alysson.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.alysson.cursomc.domain.Cidade;
 import com.alysson.cursomc.domain.Cliente;
+import com.alysson.cursomc.domain.Endereco;
+import com.alysson.cursomc.domain.enums.TipoCliente;
 import com.alysson.cursomc.dto.ClienteDTO;
+import com.alysson.cursomc.dto.ClienteNewDTO;
 import com.alysson.cursomc.repositories.ClienteRepository;
+import com.alysson.cursomc.repositories.EnderecoRepository;
 import com.alysson.cursomc.services.exceptions.DataIntegrityException;
 import com.alysson.cursomc.services.exceptions.ObjectNotFoundException;
+
+
 
 @Service
 public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -29,13 +39,18 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return repo.save(obj);
+	}
+	
 	public Cliente update(Cliente obj) {
 		Cliente oldObj = find(obj.getId());
 		obj = updateData(obj, oldObj);
 		return repo.save(obj);
 	}
-
-
 
 	public void delete(Integer id) {
 		try {
@@ -56,6 +71,20 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(null, objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli =  new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), new Cidade(objDTO.getCidadeId(), null, null), cli);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if(objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if(objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		return cli;
 	}
 	
 	private Cliente updateData(Cliente obj, Cliente oldObj) {
