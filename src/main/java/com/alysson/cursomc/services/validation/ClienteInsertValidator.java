a67377f5ac71dc5473f7437754aaa76adb466cd9
Alysson Kelvim <akcsalysson@gmail.com>
2020-04-25 14:39:53 -0300

    CRUD: Validação de E-mail existente

--- src/main/java/com/alysson/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/alysson/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alysson.cursomc.domain.Cliente;
 import com.alysson.cursomc.domain.enums.TipoCliente;
 import com.alysson.cursomc.dto.ClienteNewDTO;
+import com.alysson.cursomc.repositories.ClienteRepository;
 import com.alysson.cursomc.resources.exceptions.FieldMessage;
 import com.alysson.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 
@@ -31,6 +38,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 		}
 
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
