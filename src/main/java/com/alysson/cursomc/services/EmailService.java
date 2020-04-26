d8fb24451bcb9e5f0a41b2a7be1ea92fbd366e05
Alysson Kelvim <akcsalysson@gmail.com>
2020-04-26 00:50:39 -0300

    Email: MockMailService com Logger

---
+++ src/main/java/com/alysson/cursomc/services/EmailService.java
@@ -0,0 +1,13 @@
+package com.alysson.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.alysson.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	void sendOrderConfirmationEmail(Pedido obj) ;
+	
+	void sendEmail(SimpleMailMessage msg);
+	
+}
